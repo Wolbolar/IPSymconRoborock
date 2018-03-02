6b09ec61b2ec717ebc10bf3be5508ec15c5cfb69
Frank <frank@codeking.de>
2018-03-02 01:22:41 +0100

    shell script updated

--- libs/symcon.mapupload.sh
+++ libs/symcon.mapupload.sh
@@ -5,6 +5,16 @@ WEBHOOK=
 INSTANCE_ID=
 RUN_BY_CRONJOB=0
 
+# map data
+MAP_FILE=$(find /run/shm -type f -name "*.ppm" | head -1)
+MAP_COORDINATES=/run/shm/SLAM_fprintf.log
+
+# variables
+PNM=$(command -v pnmtopng)
+BASEDIR=$(dirname -- $(readlink -e "$0"))
+MAP_FOLDER=$(dirname -- "$MAP_FILE")
+MAP_IMAGE="$MAP_FOLDER/latest.png"
+
 # handle arguments
 for i in "$@"
 do
@@ -25,32 +35,31 @@ esac
 done
 
 if [ -z $WEBHOOK ] || [ -z $INSTANCE_ID ]; then
-    echo ""
-    echo "# IP-Symcon Map Uploader"
-    echo ""
-    echo "Usage: $0 --id=<Instance ID> --webhook=<Webhook URL>"
-    echo ""
+        chmod +x $0
 
-    exit;
+        echo ""
+        echo "# IP-Symcon Map Uploader"
+        echo ""
+
+        while [ -z $INSTANCE_ID ]
+        do
+                echo -n 'Instance ID (e.g. 12345): '
+                read INSTANCE_ID
+        done
+
+        while [ -z $WEBHOOK ]
+        do
+                echo -n 'Webhook URL (e.g. http://10.0.0.1:3777/hook/Roborock): '
+                read  WEBHOOK
+        done
 fi
-
-# map data
-echo "find current map file..."
-MAP_FILE=$(find /run/shm -type f -name "*.ppm" | head -1)
-MAP_COORDINATES=/run/shm/SLAM_fprintf.log
-
-# variables
-PNM=$(command -v pnmtopng)
-BASEDIR=$(dirname -- $(readlink -e "$0"))
-MAP_FOLDER=$(dirname -- "$MAP_FILE")
-MAP_IMAGE="$MAP_FOLDER/latest.png"
 
 # install dependencies
 if [ -z $PNM ]; then
-    echo "Updating apt index..."
+    echo "updating apt index..."
     apt-get update &> /dev/null
 
-    echo "Installing dependencies...";
+    echo "installing dependencies...";
     apt-get -y install netpbm
 fi
 
@@ -61,16 +70,25 @@ if [ $RUN_BY_CRONJOB -eq 0 ]; then
     echo "* * * * * root bash $BASEDIR/$CRON_SCRIPT --id=$INSTANCE_ID --webhook=$WEBHOOK --cronjob=1 &> /dev/null" > /etc/cron.d/symcon_mapupload
 fi
 
-# check image
-if [ -z $MAP_FILE ]; then
-    echo 'no map file found, exiting...';
-    exit
-fi
+# while loop, to execute map upload multiple times
+i=0
+while [ $i -lt 6 ]; do
+        # check image
+        if [ -z $MAP_FILE ]; then
+            echo 'no map file found, exiting...';
+            exit
+        fi
 
-# convert image to jpg
-echo "converting ppm to png..."
-$PNM -transparent rgb:7D/7D/7D $MAP_FILE &> /dev/null > $MAP_IMAGE
+        # convert image to jpg
+        echo "converting ppm to png..."
+        $PNM -transparent rgb:7D/7D/7D $MAP_FILE &> /dev/null > $MAP_IMAGE
 
-# upload to webhook
-echo "uploading to webhook..."
-curl -F "image=@$MAP_IMAGE" -F "coordinates=@$MAP_COORDINATES" "$WEBHOOK?id=$INSTANCE_ID"
+        # upload to webhook
+        echo "uploading to webhook... $WEBHOOK?id=$INSTANCE_ID"
+        curl -F "image=@$MAP_IMAGE" -F "coordinates=@$MAP_COORDINATES" "$WEBHOOK?id=$INSTANCE_ID"
+
+        # sleep for 10 seconds
+        echo "waiting 10 seconds..."
+        sleep 10
+        i=$(( i + 1 ))
+done
